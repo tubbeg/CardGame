717baf60ebd2d32b3e1665e4507f83039d848486
TobiasG <tubbeg@gmail.com>
2023-03-18 21:18:20 +0100

    Removed js builder and created new function in Config.fs

--- src/App.fs
+++ src/App.fs
@@ -19,12 +19,9 @@ type PhaserGame (config : obj) =
     class
     end
 
-type IBuildConfig = obj
 
-[<Import("buildConfig", "./BuildPhaserConfig.js")>]
-let configBuilder (scene : obj) : IBuildConfig = jsNative
 
 let testSceneExt = new SceneExt()
 let testGame2 =
-    new PhaserGame(configBuilder testSceneExt)
+    new PhaserGame(buildConfig testPhaserExtension)
 
--- src/Config.fs
+++ src/Config.fs
@@ -6,35 +6,21 @@ open Fable.Core
 open Fable.Core.JsInterop
 open PhaserSceneExtension
 
-
-type PhaserGameConfig = Config of obj
-
-let createGravity =
-    createObj [
-        "y" ==> 300
-    ]
-
-let createArcade =
-    createObj [
-        "gravity" ==> createGravity
-        "debug" ==> false
-    ]
-
-let createPhysics =
-    createObj [
+let buildConfig scene = createObj [
+    "type" ==> Auto
+    "width" ==> 800
+    "height" ==> 600
+    "physics" ==> createObj [
         "default" ==> "arcade"
-        "arcade" ==> createArcade
+        "arcade" ==> createObj [
+            "gravity" ==> createObj [
+                "y" ==> 300
+            ]
+            "debug" ==> false
+        ]
     ]
-    
-let phaserGameConfig (scene : SceneExt) = 
-    createObj [
-        "type" ==> Auto
-        "width" ==> 800
-        "height" ==> 600
-        "physics" ==> createPhysics
-        "scene" ==> scene
-    ]
-
+    "scene" ==> scene
+]
 
 
 
